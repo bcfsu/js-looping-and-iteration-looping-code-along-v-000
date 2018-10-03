b23d79e272223eee337e7592ffdd92bf114cb86b
Brittany <britt.cumens@gmail.com>
2018-10-03 17:42:09 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+function printBadges(array) {
+    for (let n = 0; n < array.length; n++) {
+      console.log(`Welcome ${array[n]}! You are employee #${n + 1}.`)
+    };
+    return array
+};
+
+function tailsNeverFails() {
+  let coin = Math.random()
+  let flips = 0
+  // while (coin <= 0.5) {
+  //   flips++
+  //   // coin = Math.random();
+  // };
+  return `You got ${flips} tails in a row!`
+};
